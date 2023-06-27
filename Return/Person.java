5a96da73439ec26fb804a3346e2bb5a3c7a7639a
Suraj S Neelagar <surajneelagar@gmail.com>
2023-06-27 13:53:29 +0530

    Return type

---
+++ Return/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+    static String getName(String name) {
+        // System.out.print("My name is : " + name);
+        return name;
+    }
+    static int getAge(int age) {
+        System.out.println("Age is : " + age);
+        return age;
+    }
+    static String getEmail(String email) {
+        System.out.println("Email is : " + email);
+        return email;
+    }
+    static long getMobileNo(long phone) {
+        System.out.println("Phone number is : " + phone);
+        return phone;
+    }
+    static double getHeight(double height) {
+        System.out.println("Height is : " + height);
+        return height;
+    }
+    static double getWeight(double weight) {
+        System.out.println("Weight is : " + weight);
+        return weight;
+    }
+    static double getSalary(double salary) {
+        System.out.println("Salary is : " + salary);
+        return salary;
+    }
+    static String getBestFriendName(String bestFriend) {
+        System.out.println("Best friend is : " + bestFriend);
+        return bestFriend;
+    }
+    static String getFatherName(String fatherName) {
+        System.out.println("Father is : " + fatherName);
+        return fatherName;
+    }
+    static String getMotherName(String motherName) {
+        System.out.println("Mother is : " + motherName);
+        return motherName;
+    }
+    static String getAreaName(String areaName) {
+        System.out.println("Area name is : " + areaName);
+        return areaName;
+    }
+    static String getEnemy(String enemy) {
+        System.out.println("Enemy is : " + enemy);
+        return enemy;
+    }
+    static String getInspiration(String inspiration) {
+        System.out.println("Inspiration is : " + inspiration);
+        return inspiration;
+    }
+}
---
+++ Return/PersonRunner.java
@@ -0,0 +1,33 @@
+public class PersonRunner {
+    public static void main(String[] args) {
+
+        String name = Person.getName("Suraj");
+        System.out.println("My name is : "+  name);
+
+        Person.getAge(23);
+
+        Person.getEmail("surajneelager.xworkz@gmail.com");
+
+        Person.getMobileNo(1234560789);
+
+        Person.getHeight(144);
+
+        Person.getWeight(50.5);
+
+        Person.getSalary(1000000.00);
+
+        Person.getBestFriendName("Manjunath");
+
+        Person.getFatherName("Shashidhar");
+
+        Person.getMotherName("Surekha");
+
+        Person.getAreaName("Dharwad");
+
+        Person.getEnemy("No One");
+
+        Person.getInspiration("Virat Kohil");
+
+
+    }
+}
