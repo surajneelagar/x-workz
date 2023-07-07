876c8eff425ad91e9e10e7bef082cd5003906b82
Suraj S Neelagar <surajneelagar@gmail.com>
2023-07-07 23:19:31 +0530

    chaining

---
+++ Chaining 2/Sofa.java
@@ -0,0 +1,23 @@
+class Sofa{
+    String brand;
+    double price;
+    int capcity;
+    String color;
+    String type;
+    double weight;
+	public String wood;
+
+
+
+    Sofa(String brand,double price,int capcity,String color, String type,double weight)
+    {
+        System.out.println("Constructor is  running.....");
+        this.brand = brand;
+        this.price=price ;
+        this.capcity=capcity ;
+        this.color=color ;
+        this.type=type ;
+        this.weight=weight ;
+    }
+
+}
---
+++ Chaining 2/SofaRunner.java
@@ -0,0 +1,85 @@
+public class SofaRunner {
+    public static void main(String[] args) {
+        System.out.println("Running main");
+
+        Sofa sofa1 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa1.brand);
+		System.out.println("Sofa length in mtr :" + sofa1.price);
+		System.out.println("sofa color :" +  sofa1.capcity);
+		System.out.println("Sofa made of :" + sofa1.color);
+		System.out.println("Sofa Capacity :" +sofa1.type);
+		System.out.println("Sofa purpose :" +sofa1.wood);
+		System.out.println("Sofa weight :" +sofa1.weight);
+		System.out.println();
+
+		Sofa sofa2 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa2.brand);
+		System.out.println("Sofa length in mtr :" + sofa2.price);
+		System.out.println("sofa color :" +  sofa2.capcity);
+		System.out.println("Sofa made of :" + sofa2.color);
+		System.out.println("Sofa Capacity :" +sofa2.type);
+		System.out.println("Sofa purpose :" +sofa2.wood);
+		System.out.println("Sofa weight :" +sofa2.weight);
+		System.out.println();
+
+		Sofa sofa3 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa3.brand);
+		System.out.println("Sofa length in mtr :" + sofa3.price);
+		System.out.println("sofa color :" +  sofa3.capcity);
+		System.out.println("Sofa made of :" + sofa3.color);
+		System.out.println("Sofa Capacity :" +sofa3.type);
+		System.out.println("Sofa purpose :" +sofa3.wood);
+		System.out.println("Sofa weight :" +sofa3.weight);
+		System.out.println();
+
+		Sofa sofa4 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa4.brand);
+		System.out.println("Sofa length in mtr :" + sofa4.price);
+		System.out.println("sofa color :" +  sofa4.capcity);
+		System.out.println("Sofa made of :" + sofa4.color);
+		System.out.println("Sofa Capacity :" +sofa4.type);
+		System.out.println("Sofa purpose :" +sofa4.wood);
+		System.out.println("Sofa weight :" +sofa4.weight);
+		System.out.println();
+
+		Sofa sofa5 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa5.brand);
+		System.out.println("Sofa length in mtr :" + sofa5.price);
+		System.out.println("sofa color :" +  sofa5.capcity);
+		System.out.println("Sofa made of :" + sofa5.color);
+		System.out.println("Sofa Capacity :" +sofa5.type);
+		System.out.println("Sofa purpose :" +sofa5.wood);
+		System.out.println("Sofa weight :" +sofa5.weight);
+		System.out.println();
+
+		Sofa sofa6 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa6.brand);
+		System.out.println("Sofa length in mtr :" + sofa6.price);
+		System.out.println("sofa color :" +  sofa6.capcity);
+		System.out.println("Sofa made of :" + sofa6.color);
+		System.out.println("Sofa Capacity :" +sofa6.type);
+		System.out.println("Sofa purpose :" +sofa6.wood);
+		System.out.println("Sofa weight :" +sofa6.weight);
+		System.out.println();
+
+		Sofa sofa7 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa7.brand);
+		System.out.println("Sofa length in mtr :" + sofa7.price);
+		System.out.println("sofa color :" +  sofa7.capcity);
+		System.out.println("Sofa made of :" + sofa7.color);
+		System.out.println("Sofa Capacity :" +sofa7.type);
+		System.out.println("Sofa purpose :" +sofa7.wood);
+		System.out.println("Sofa weight :" +sofa7.weight);
+		System.out.println();
+
+		Sofa sofa8 = new Sofa("Adorn India", 29000.0, 6,"red","wood",50);
+		System.out.println("Brand:" + sofa8.brand);
+		System.out.println("Sofa length in mtr :" + sofa8.price);
+		System.out.println("sofa color :" +  sofa8.capcity);
+		System.out.println("Sofa made of :" + sofa8.color);
+		System.out.println("Sofa Capacity :" +sofa8.type);
+		System.out.println("Sofa purpose :" +sofa8.wood);
+		System.out.println("Sofa weight :" +sofa8.weight);
+		System.out.println();
+    }
+}
